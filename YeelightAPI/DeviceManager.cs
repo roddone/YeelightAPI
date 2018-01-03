19b8c8c9e0254b6fcb1fdbf3a7c09114ec4a4aea
romain oddone <romain.oddone@outlook.com>
2018-01-03 19:03:41 +0100

    allow to connect to a hostname or IP adress

--- YeelightAPI/DeviceManager.cs
+++ YeelightAPI/DeviceManager.cs
@@ -33,12 +33,12 @@ namespace YeelightAPI
         /// </summary>
         /// <param name="device"></param>
         /// <returns></returns>
-        public bool Connect(string ip, int port = 55443)
+        public bool Connect(string hostname, int port = 55443)
         {
             this.Disconnect();
 
             this.tcpClient = new TcpClient();
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            IPEndPoint endPoint = GetIPEndPointFromHostName(hostname, 55443);
             this.tcpClient.Connect(endPoint);
 
             if (!this.tcpClient.Connected)
@@ -268,6 +268,32 @@ namespace YeelightAPI
                          .Cast<PROPERTIES>()
                          .Select(x => x.GetRealName())
                          .ToList<object>();
+        }
+
+        /// <summary>
+        /// Get an ip from a hostname
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        private static IPEndPoint GetIPEndPointFromHostName(string hostName, int port)
+        {
+            var addresses = System.Net.Dns.GetHostAddresses(hostName);
+            if (addresses.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Unable to retrieve address from specified host name.",
+                    "hostName"
+                );
+            }
+            else if (addresses.Length > 1)
+            {
+                throw new ArgumentException(
+                    "There is more that one IP address to the specified host.",
+                    "hostName"
+                );
+            }
+            return new IPEndPoint(addresses[0], port); // Port gets validated here.
         }
     }
 }
