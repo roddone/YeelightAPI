9a5c5a2674459135eae845acd7ebcbe705b650f1
Romain ODDONE <roddone@publicis-eto.fr>
2018-02-01 13:52:13 +0100

    reordering files

---
+++ YeelightAPI/Events/ErrorEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YeelightAPI.Models;
+
+namespace YeelightAPI
+{
+    /// <summary>
+    /// Notification event Argument
+    /// </summary>
+    public class ErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ErrorEventArgs() { }
+
+        /// <summary>
+        /// Constructor with notification result
+        /// </summary>
+        /// <param name="result"></param>
+        public ErrorEventArgs(CommandResult.CommandErrorResult result)
+        {
+            this.Result = result;
+        }
+
+        /// <summary>
+        /// Notification Result
+        /// </summary>
+        public CommandResult.CommandErrorResult Result { get; set; }
+    }
+}
