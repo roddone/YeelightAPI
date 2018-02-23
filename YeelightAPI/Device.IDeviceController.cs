bafeae9baf019bd2b0e4f171cbfc1adf4e4caae0
Romain ODDONE <romain.oddone@outlook.com>
2018-02-23 11:31:56 +0100

    code cleanup

--- YeelightAPI/Device.IDeviceController.cs
+++ YeelightAPI/Device.IDeviceController.cs
@@ -13,18 +13,7 @@ namespace YeelightAPI
     /// </summary>
     public partial class Device : IDeviceController
     {
-        /// <summary>
-        /// Disconnect the current device
-        /// </summary>
-        /// <returns></returns>
-        public void Disconnect()
-        {
-            if (_tcpClient != null)
-            {
-                _tcpClient.Close();
-                _tcpClient = null;
-            }
-        }
+        #region Public Methods
 
         /// <summary>
         /// Connects to a device asynchronously
@@ -64,28 +53,67 @@ namespace YeelightAPI
         }
 
         /// <summary>
-        /// Toggle the device power asynchronously
+        /// Add a cron job
         /// </summary>
+        /// <param name="value"></param>
+        /// <param name="type"></param>
         /// <returns></returns>
-        public async Task<bool> Toggle()
+        public async Task<bool> CronAdd(int value, CronType type = CronType.PowerOff)
         {
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(METHODS.Toggle, id: (int)METHODS.Toggle);
+            List<object> parameters = new List<object>() { (int)type, value };
+
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
+                method: METHODS.AddCron,
+                id: (int)METHODS.AddCron,
+                parameters: parameters);
 
             return result.IsOk();
         }
 
         /// <summary>
-        /// Set the device power state asynchronously
+        /// Delete a cron job
         /// </summary>
-        /// <param name="state"></param>
+        /// <param name="type"></param>
         /// <returns></returns>
-        public async Task<bool> SetPower(bool state = true)
+        public async Task<bool> CronDelete(CronType type = CronType.PowerOff)
         {
+            List<object> parameters = new List<object>() { (int)type };
+
             CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
-                method: METHODS.SetPower,
-                id: (int)METHODS.SetPower,
-                parameters: new List<object>() { state ? "on" : "off" }
-            );
+                method: METHODS.DeleteCron,
+                id: (int)METHODS.DeleteCron,
+                parameters: parameters);
+
+            return result.IsOk();
+        }
+
+        /// <summary>
+        /// Disconnect the current device
+        /// </summary>
+        /// <returns></returns>
+        public void Disconnect()
+        {
+            if (_tcpClient != null)
+            {
+                _tcpClient.Close();
+                _tcpClient = null;
+            }
+        }
+
+        /// <summary>
+        /// Adjusts the state of the device
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public async Task<bool> SetAdjust(AdjustAction action, AdjustProperty property)
+        {
+            List<object> parameters = new List<object>() { action.ToString(), property.ToString() };
+
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
+                method: METHODS.SetAdjust,
+                id: (int)METHODS.SetAdjust,
+                parameters: parameters);
 
             return result.IsOk();
         }
@@ -105,30 +133,6 @@ namespace YeelightAPI
             CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
                 method: METHODS.SetBrightness,
                 id: (int)METHODS.SetBrightness,
-                parameters: parameters);
-
-            return result.IsOk();
-        }
-
-        /// <summary>
-        /// Change the device RGB color asynchronously
-        /// </summary>
-        /// <param name="r"></param>
-        /// <param name="g"></param>
-        /// <param name="b"></param>
-        /// <param name="smooth"></param>
-        /// <returns></returns>
-        public async Task<bool> SetRGBColor(int r, int g, int b, int? smooth = null)
-        {
-            //Convert RGB into integer 0x00RRGGBB
-            int value = ((r) << 16) | ((g) << 8) | (b);
-            List<object> parameters = new List<object>() { value };
-
-            HandleSmoothValue(ref parameters, smooth);
-
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
-                method: METHODS.SetRGBColor,
-                id: (int)METHODS.SetRGBColor,
                 parameters: parameters);
 
             return result.IsOk();
@@ -155,6 +159,17 @@ namespace YeelightAPI
         }
 
         /// <summary>
+        /// Set the current state as the default one
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> SetDefault()
+        {
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(METHODS.SetDefault, id: (int)METHODS.SetDefault);
+
+            return result.IsOk();
+        }
+
+        /// <summary>
         /// Change HSV color asynchronously
         /// </summary>
         /// <param name="hue"></param>
@@ -170,6 +185,46 @@ namespace YeelightAPI
             CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
                 method: METHODS.SetHSVColor,
                 id: (int)METHODS.SetHSVColor,
+                parameters: parameters);
+
+            return result.IsOk();
+        }
+
+        /// <summary>
+        /// Set the device power state asynchronously
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public async Task<bool> SetPower(bool state = true)
+        {
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
+                method: METHODS.SetPower,
+                id: (int)METHODS.SetPower,
+                parameters: new List<object>() { state ? "on" : "off" }
+            );
+
+            return result.IsOk();
+        }
+
+        /// <summary>
+        /// Change the device RGB color asynchronously
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="g"></param>
+        /// <param name="b"></param>
+        /// <param name="smooth"></param>
+        /// <returns></returns>
+        public async Task<bool> SetRGBColor(int r, int g, int b, int? smooth = null)
+        {
+            //Convert RGB into integer 0x00RRGGBB
+            int value = ((r) << 16) | ((g) << 8) | (b);
+            List<object> parameters = new List<object>() { value };
+
+            HandleSmoothValue(ref parameters, smooth);
+
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
+                method: METHODS.SetRGBColor,
+                id: (int)METHODS.SetRGBColor,
                 parameters: parameters);
 
             return result.IsOk();
@@ -208,63 +263,16 @@ namespace YeelightAPI
         }
 
         /// <summary>
-        /// Adjusts the state of the device
+        /// Toggle the device power asynchronously
         /// </summary>
-        /// <param name="action"></param>
-        /// <param name="property"></param>
         /// <returns></returns>
-        public async Task<bool> SetAdjust(AdjustAction action, AdjustProperty property)
+        public async Task<bool> Toggle()
         {
-            List<object> parameters = new List<object>() { action.ToString(), property.ToString() };
-
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
-                method: METHODS.SetAdjust,
-                id: (int)METHODS.SetAdjust,
-                parameters: parameters);
+            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(METHODS.Toggle, id: (int)METHODS.Toggle);
 
             return result.IsOk();
         }
 
-        /// <summary>
-        /// Add a cron job
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public async Task<bool> CronAdd(int value, CronType type = CronType.PowerOff)
-        {
-            List<object> parameters = new List<object>() { (int)type, value };
-
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
-                method: METHODS.AddCron,
-                id: (int)METHODS.AddCron,
-                parameters: parameters);
-
-            return result.IsOk();
-        }
-
-        /// <summary>
-        /// Delete a cron job
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public async Task<bool> CronDelete(CronType type = CronType.PowerOff)
-        {
-            List<object> parameters = new List<object>() { (int)type };
-
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(
-                method: METHODS.DeleteCron,
-                id: (int)METHODS.DeleteCron,
-                parameters: parameters);
-
-            return result.IsOk();
-        }
-
-        public async Task<bool> SetDefault()
-        {
-            CommandResult<List<string>> result = await ExecuteCommandWithResponse<List<string>>(METHODS.SetDefault, id: (int)METHODS.SetDefault);
-
-            return result.IsOk();
-        }
+        #endregion Public Methods
     }
 }
