4b29c6b23a6c61ca6ad22a5e1ff564fb4aeb18a7
romain oddone <romain.oddone@outlook.com>
2020-08-07 16:01:18 +0200

    add comments

--- YeelightAPI/Core/DiscoveryResult.cs
+++ YeelightAPI/Core/DiscoveryResult.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace YeelightAPI.Core
 {
+    /// <summary>
+    /// Results of a discovery
+    /// </summary>
     internal class DiscoveryResult
     {
+        /// <summary>
+        /// Devices found during the discovery
+        /// </summary>
         internal IEnumerable<Device> Devices { get; set; }
 
+        /// <summary>
+        /// Exceptions thrown during discovery
+        /// </summary>
         internal IEnumerable<DeviceDiscoveryException> Exceptions { get; set; }
     }
 }
